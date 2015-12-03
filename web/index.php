26e93f766bb9488dfc4cf344e1a258a0be6572a8
asap <moreno.funari@superscommesse.it>
2015-12-03 13:08:09 +0100

    primo repository del sito

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<div> CIAO MONDO </div>
+<?php
+
+
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+sfContext::createInstance($configuration)->dispatch();
